132dfc2d557c4995baff181ffe0f982b9032da0a
kimjiwon506 <kimjiwon506@gmail.com>
2022-08-26 13:00:15 +0900

    뉴스뷰어 쿼리연동

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import NewsList from './component/NewsList';
+import Categories from './component/Categories';
+import { useState,useCallback } from 'react';
 
 function App() {
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback(category=>setCategory(category),[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Categories category={category} onSelect={onSelect} />
+    <NewsList category={category} />
+    </>
   );
 }
 
